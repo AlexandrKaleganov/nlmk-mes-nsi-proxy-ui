376a67aa1f05dbde5730fef928e3fa516c0be2c4
Alexander Kaleganov <alexmur07@mail.ru>
2021-03-14 01:49:09 +0700

    init2

--- src/app/entity/supplier/supplier-routing.module.ts
+++ src/app/entity/supplier/supplier-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {SupplierComponent} from './supplier.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'supplier',
+  children: [
+    {path: '', component: SupplierComponent}],
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class SupplierRoutingModule { }
+export class SupplierRoutingModule {
+}
