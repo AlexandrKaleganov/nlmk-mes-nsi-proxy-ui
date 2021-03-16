c41d68cde23eb108567ea932c0a745b472f4f5a7
Alexander Kaleganov <alexmur07@mail.ru>
2021-03-16 20:41:21 +0700

    Типы материальных ресурсов

--- src/app/entity/resource/filter-resource/filter-resource.component.ts
+++ src/app/entity/resource/filter-resource/filter-resource.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {MarkResource} from '../../../shared/models/mark-resource.class';
+import {TypeResource} from '../../../shared/models/type-resource.model';
+import {MarkResourceService} from '../../mark-resource/mark-resource.service';
+import {TypeResourceService} from '../../type-resource/type-resource.service';
 
 @Component({
   selector: 'app-filter-resource',
@@ -9,7 +13,19 @@ import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 export class FilterResourceComponent implements OnInit {
   codeFilter: string;
   nameFilter: string;
-  constructor(public activeModal: NgbActiveModal) {
+  markResourceService: MarkResourceService;
+  typeResourceService: TypeResourceService;
+
+  materialResourceMarkList: MarkResource[] = [];
+  materialResourceTypeList: TypeResource[] = [];
+
+  materialResourceMarkId: string;
+  materialResourceTypeId: string;
+
+
+  constructor(public activeModal: NgbActiveModal, markResourceService: MarkResourceService, typeResourceService: TypeResourceService) {
+    this.markResourceService = markResourceService;
+    this.typeResourceService = typeResourceService;
   }
 
   ngOnInit(): void {
@@ -19,10 +35,29 @@ export class FilterResourceComponent implements OnInit {
     this.activeModal.close();
   }
 
+  loadDirectories(): void {
+    this.markResourceService.findAllList().subscribe(res => {
+      this.materialResourceMarkList = res.body;
+    });
+    this.typeResourceService.findAllList().subscribe(res => {
+      this.materialResourceTypeList = res.body;
+    });
+  }
+
+  trackByIdMark(index: number, item: MarkResource): any {
+    return item.id;
+  }
+
+  trackByIdType(index: number, item: TypeResource): any {
+    return item.id;
+  }
+
   filter(): void {
     this.activeModal.close({
       codeFilter: this.codeFilter,
-      nameFilter: this.nameFilter ,
+      nameFilter: this.nameFilter,
+      materialResourceMarkId: this.materialResourceMarkId,
+      materialResourceTypeId: this.materialResourceTypeId,
     });
   }
 }
--- src/app/entity/resource/resource.component.ts
+++ src/app/entity/resource/resource.component.ts
@@ -20,6 +20,8 @@ export class ResourceComponent implements OnInit {
   page = 1;
   codeFilter: string;
   nameFilter: string;
+  materialResourceMarkId: string;
+  materialResourceTypeId: string;
 
   constructor(resourceService1: ResourceService, public modalService: NgbModal) {
     this.resourceService = resourceService1;
@@ -39,6 +41,12 @@ export class ResourceComponent implements OnInit {
     options = options.set('sort', 'name');
     if (this.codeFilter) {
       options = options.set('code.contains', this.codeFilter);
+    }
+    if (this.materialResourceMarkId) {
+      options = options.set('materialResourceMarkId.equals', this.materialResourceMarkId);
+    }
+    if (this.materialResourceTypeId) {
+      options = options.set('materialResourceTypeId.equals', this.materialResourceTypeId);
     }
     if (this.nameFilter) {
       options = options.set('name.contains', this.nameFilter);
@@ -87,11 +95,17 @@ export class ResourceComponent implements OnInit {
     const modelRef = this.modalService.open(FilterResourceComponent, {size: 'lg', backdrop: 'static'});
     modelRef.componentInstance.codeFilter = this.codeFilter;
     modelRef.componentInstance.nameFilter = this.nameFilter;
+    modelRef.componentInstance.materialResourceMarkId = this.materialResourceMarkId;
+    modelRef.componentInstance.materialResourceTypeId = this.materialResourceTypeId;
+    modelRef.componentInstance.loadDirectories();
     modelRef.result.then(result => {
-      console.log(result);
       if (result) {
         this.codeFilter = result.codeFilter;
         this.nameFilter = result.nameFilter;
+        this.materialResourceMarkId = result.materialResourceMarkId;
+        this.materialResourceTypeId = result.materialResourceTypeId;
+        console.log(result.materialResourceMarkId);
+        console.log(result.materialResourceTypeId);
         this.loadPage(1);
       }
     });
@@ -100,6 +114,8 @@ export class ResourceComponent implements OnInit {
   deleteFilters(): void {
     this.codeFilter = null;
     this.nameFilter = null;
+    this.materialResourceMarkId = null;
+    this.materialResourceTypeId = null;
     this.loadPage(1);
   }
 }
