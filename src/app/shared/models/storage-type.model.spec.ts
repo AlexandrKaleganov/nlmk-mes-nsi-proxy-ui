9ebaf09f58fb487246b72247d8f77e6c6a8207f2
Alexander Kaleganov <alexmur07@mail.ru>
2021-03-24 11:39:54 +0700

    Нормативные документы связь

---
+++ src/app/shared/models/storage-type.model.spec.ts
@@ -0,0 +1,7 @@
+import { StorageType } from './storage-type.model';
+
+describe('StorageType', () => {
+  it('should create an instance', () => {
+    expect(new StorageType()).toBeTruthy();
+  });
+});
